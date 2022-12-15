1f363b2cf3f8fc8f55d24a4fd30dcbd866c7bc9e
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-12-14 20:48:25 -0800

    added comment, header, foot components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        &copy;{new Date().getFullYear()} by David, Kevin, Lauren, Rachel
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
