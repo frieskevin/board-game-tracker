b89f0e9ef9351cbd70a9c6f09ac3e052a28d37c9
alotofsnakes <mrfries@gmail.com>
2022-12-14 21:01:02 -0800

    added queries and mutations on front end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,92 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      games {
+        _id
+        title
+        username
+        winner
+        score
+        gameNotes
+        createdAt
+        comments {
+            commentBody
+            username
+            createdAt
+        }
+        image
+        link
+      }
+    }
+}`;
+
+export const QUERY_USERS = gql`
+  query users {
+    users {
+      _id
+      name
+      email
+    }
+}`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+        _id
+        username
+        email
+        games {
+          _id
+          title
+          winner
+          gameNotes
+          }
+        }
+    }`;
+
+export const QUERY_GAMES = gql`
+  query games($username: String!) {
+    games(username: $username) {
+        _id
+        title
+        username
+        winner
+        score
+        gameNotes
+        createdAt
+        comments {
+            commentBody
+            username
+            createdAt
+        }
+        image
+        link
+    }
+}`;
+
+export const QUERY_GAME = gql`
+    query game($_id: ID) {
+        game(_id: $_id) {
+            _id
+            title
+            username
+            winner
+            score
+            gameNotes
+            createdAt
+            comments {
+                commentBody
+                username
+                createdAt
+            }
+            image
+            link
+    
+        }
+    }`;
+
