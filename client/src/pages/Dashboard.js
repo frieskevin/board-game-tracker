dde21d11a9d6baa5462eb8352d2e776e2a60ac21
alotofsnakes <mrfries@gmail.com>
2022-12-17 11:51:52 -0800

    games display on dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_GAMES } from '../utils/queries';
+import { QUERY_ME } from '../utils/queries';
 import GameList from '../components/GameList';
 import Auth from '../utils/auth';
 import AddGameModal from '../components/Modal/AddGameModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Dashboard = () => {
-    const { loading, data } = useQuery(QUERY_GAMES);
+    const { username: userParam } = useParams();
+    const { loading, data } = useQuery(QUERY_ME, { variables: { username: userParam}});
     const loggedIn = Auth.loggedIn();
-    const games = data?.games || [];
-
+    const me = data?.me || [];
+    console.log(me);
   
     return (
         <main>
@@ -24,7 +26,7 @@ const Dashboard = () => {
                     <div>Loading...</div>
                 ) : (
                     <GameList
-                        games={games}
+                        games={me.games}
                         title="Games Played"
                     />
                 )}
