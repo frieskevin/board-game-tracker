127ef013727fc5b081dcc9d62dcd2cc23f3ccafb
alotofsnakes <mrfries@gmail.com>
2022-12-19 22:01:50 -0800

    moved install logic

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -22,6 +22,23 @@ function Header(args) {
     Auth.logout();
   };
 
+  const installBtn = document.getElementById('installBtn');
+
+  window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+
+    installBtn.addEventListener('click', () => {
+      event.prompt();
+      installBtn.setAttribute('disabled', true);
+      installBtn.textContent = 'Installed!';
+    });
+  });
+
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+  });
+
   return (
     <div >
       <Navbar {...args} className="nav-background">
